a729fb9e46c4ce180a85d66be01976d98f65d55b
Stefan Aleksandrov <arh.inj.alexandrov@abv.bg>
2021-02-23 18:35:43 +0200

    Update error handler

--- js-backend/2021.02.19-Tutorials/middlewares/globalErrorHandler.js
+++ js-backend/2021.02.19-Tutorials/middlewares/globalErrorHandler.js
@@ -1,8 +1,15 @@
 const globalErrorHandler = function (err, req, res, next) {
+    const views = {
+        '/auth/register': 'register',
+        '/auth/login': 'login',
+        '/course/create': 'createCourse',
+    }
+
     err.message = err.message || 'Something went wrong';
     err.status = err.status || 500;
+    let view = views[req.path] || 'home'
 
-    res.status(err.status).render('home', { layouts: 'main', error: err });
+    res.status(err.status).render(view, { layouts: 'main', error: err });
 }
 
 module.exports = globalErrorHandler;
